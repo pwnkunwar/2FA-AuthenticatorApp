ca9834c164fd87d60a29b73e1b1c960e5651c23c
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-02 21:12:04 +0545

    Initial Commit

---
+++ 2FA-AuthenticatorApp/Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2FA_AuthenticatorApp.Database
+{
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
