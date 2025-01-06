9e859f48f39ad8a4b1e88704783d111f60090245
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-06 21:45:11 +0545

    OnPostEnable2FaAsync, OnPostGenerateRecoveryCodeAsync,FormatKey methods

--- 2FA-AuthenticatorApp/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ 2FA-AuthenticatorApp/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -31,7 +31,8 @@ namespace TwoFA_AuthenticatorApp.Areas.Identity.Pages.Account.Manage
         public bool IsMachineRemembered { get; set; }
         [TempData]
         public string StatusMessage { get; set; }
-
+        [BindProperty]
+        public string VerificationCode { get; set; }
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -60,5 +61,40 @@ namespace TwoFA_AuthenticatorApp.Areas.Identity.Pages.Account.Manage
             StatusMessage = "The current browser has been forgotten. When you login again from this browser you will be prompted for your 2fa code.";
             return RedirectToPage();
         }
+        public async Task<IActionResult> OnPostEnable2FaAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if(user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            var isValid = await _userManager.VerifyTwoFactorTokenAsync(user, _userManager.Options.Tokens.AuthenticatorTokenProvider, VerificationCode);
+            if(!isValid)
+            {
+                ModelState.AddModelError("VerificationCode", "Verification code is invalid.");
+                return Page();
+            }
+            await _userManager.SetTwoFactorEnabledAsync(user, false);
+            StatusMessage = "Two-factor authentication has been disabled.";
+            return RedirectToPage();
+        }
+        public async Task<IActionResult> OnPostGenerateRecoveryCodeAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if(user == null)
+            {
+                return NotFound($"Unable to load user eith ID '{_userManager.GetUserId(User)}'.");  
+            }
+            var recoveryCodes = await _userManager.GenerateNewTwoFactorRecoveryCodesAsync(user, 10);
+            RecoveryCodesLeft = recoveryCodes.Count();
+            StatusMessage = "Recovery codes have been generated.";
+            return RedirectToPage(new {recoveryCodes});
+        }
+
+        private string FormatKey(string key)
+            {
+                return string.Join(" ", key?.Chunk(4) ?? Array.Empty<char[]>());
+            }
+
     }
 }
